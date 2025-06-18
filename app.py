1a81e859ce87e020ce0ceebfbf21ad2a7d365fe6
mhmmddzaky <muh.dzaky1919@gmail.com>
2025-06-18 22:03:24 +0700

    tambah fitur tombol

--- app.py
+++ app.py
@@ -16,3 +16,10 @@ df = pd.read_csv(data_path, sep=';')
 # Tampilkan dataset
 st.subheader("Dataset")
 st.dataframe(df.head())
+
+st.subheader("🎯 Tampilkan Data Mahasiswa dengan Status Enrolled")
+
+# Tombol untuk memunculkan data dengan Status = 1 (Enrolled)
+if st.button("Tampilkan Mahasiswa Enrolled"):
+    enrolled_df = df[df['Status'] == 1].reset_index(drop=True)
+    st.dataframe(enrolled_df.head(20))  # Tampilkan 20 baris pertama
